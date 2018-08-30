d6800dc80188a6e5c0d34359a41f0ab0315552cc
Tom <antiflu@gmail.com>
2018-08-30 19:59:49 +0200

    Permissions

---
+++ fix-permissions.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+user=$(whoami)
+sudo chown -R $user:www-data .
+find . -type f | xargs chmod 640
+find ./bin -type f | xargs chmod 750
+find . -type d | xargs chmod 750
+find . -type d | xargs chmod +s
+find ./user/config | xargs chmod g+w
+find ./user/accounts | xargs chmod g+w
+find ./cache | xargs chmod g+w
+find ./tmp | xargs chmod g+w
+find ./user/data | xargs chmod g+w
+find ./assets | xargs chmod g+w
+find ./backup | xargs chmod g+w
+find ./images | xargs chmod g+w
+find ./logs | xargs chmod g+w
+chmod u+x *.sh
+
