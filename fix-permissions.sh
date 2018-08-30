eaf37ec4f6e79c2991d109bbe31a39cf347c7671
Tom <antiflu@gmail.com>
2018-08-30 20:47:39 +0200

    [FIX] sidebar

--- fix-permissions.sh
+++ fix-permissions.sh
@@ -7,7 +7,7 @@ find . -type d | xargs chmod 750
 find . -type d | xargs chmod +s
 find ./user/config | xargs chmod g+w
 find ./user/accounts | xargs chmod g+w
-find ./cache | xargs chmod g+w
+find ./cache | xargs chmod og+w
 find ./tmp | xargs chmod g+w
 find ./user/data | xargs chmod g+w
 find ./assets | xargs chmod g+w
